848c78aca19c90cc71c71ddbf8ecea6ac6c712d6
Rohan Gupta <rohang0072@gmail.com>
2021-07-24 13:10:36 -0400

    initial commit

---
+++ flaskblog/__init__.py
@@ -0,0 +1,44 @@
+'''
+Create flask app and SQL database here
+'''
+import typing_extensions
+from flask import Flask, config
+import sqlalchemy
+from sqlalchemy.orm import backref
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager, login_manager
+from flask_bootstrap import Bootstrap
+from flask_mail import Mail
+from flaskblog.config import Config
+
+
+mail = Mail()
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+login_manager.login_view = 'users.login' #function for login route
+login_manager.login_message_category = 'info'
+bootstrap = Bootstrap()
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    mail.init_app(app)
+    bcrypt.init_app(app)
+    bootstrap.init_app(app)
+    login_manager.init_app(app)
+
+    from flaskblog.users.routes import users
+    from flaskblog.posts.routes import posts
+    from flaskblog.main.routes import main
+    from flaskblog.errors.handlers import errors
+
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(main)
+    app.register_blueprint(errors)
+
+    return app
---
+++ flaskblog/config.py
@@ -0,0 +1,13 @@
+import json
+
+with open("config.json") as config_file:
+    config = json.load(config_file)
+
+class Config:
+    SECRET_KEY = config.get("SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = config.get("SQLALCHEMY_DATABASE_URI")
+    MAIL_SERVER = config.get("MAIL_SERVER")
+    MAIL_PORT = config.get("MAIL_PORT")
+    MAIL_USE_TLS = config.get("MAIL_USE_TLS")
+    MAIL_USERNAME = config.get("MAIL_USER")
+    MAIL_PASSWORD = config.get("MAIL_PASS")
