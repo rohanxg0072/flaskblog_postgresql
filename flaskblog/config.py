523f5bb3cf9c07ed89a2a4ff38e769f13f512006
Rohan Gupta <rohang0072@gmail.com>
2021-07-26 17:55:59 -0400

    reset to old before postgres modifications

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -5,9 +5,10 @@ with open("config.json") as config_file:
 
 class Config:
     SECRET_KEY = config.get("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = config.get("SQLALCHEMY_DATABASE_URI")
+    DATABASE_URL = config.get("DATABASE_URL")
     MAIL_SERVER = config.get("MAIL_SERVER")
     MAIL_PORT = config.get("MAIL_PORT")
     MAIL_USE_TLS = config.get("MAIL_USE_TLS")
     MAIL_USERNAME = config.get("MAIL_USER")
     MAIL_PASSWORD = config.get("MAIL_PASS")
+
