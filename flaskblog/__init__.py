298999bcb15a64b8a16e70cee0fd4e6240856262
Rohan Gupta <rohang0072@gmail.com>
2021-07-26 19:41:01 -0400

    added create tables function

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -11,9 +11,6 @@ from flask_login import LoginManager, login_manager
 from flask_bootstrap import Bootstrap
 from flask_mail import Mail
 from flaskblog.config import Config
-import click
-from flask.cli import with_appcontext
-
 
 mail = Mail()
 db = SQLAlchemy()
@@ -22,11 +19,6 @@ login_manager = LoginManager()
 login_manager.login_view = 'users.login' #function for login route
 login_manager.login_message_category = 'info'
 bootstrap = Bootstrap()
-
-@click.command(name="create_tables")
-@with_appcontext
-def create_tables():
-    db.create_all()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
--- flaskblog/models.py
+++ flaskblog/models.py
@@ -3,6 +3,8 @@ from datetime import datetime
 from flask_login import UserMixin
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app
+import click
+from flask.cli import with_appcontext
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -47,3 +49,8 @@ class Post(db.Model):
 
   def __repr__(self):
     return f"Post('{self.title}', '{self.date_posted}')"
+
+@click.command(name="create_tables")
+@with_appcontext
+def create_tables():
+    db.create_all()
