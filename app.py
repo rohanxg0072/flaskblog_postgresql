230f5945552c319c117c0022f0f0fe88368d2373
Rohan Gupta <rohang0072@gmail.com>
2021-07-26 20:13:57 -0400

    changing init file to create db in console

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-from flaskblog import create_app
+from flaskblog import app
 from dotenv import load_dotenv
 
 load_dotenv()
-app = create_app()
 
 if __name__ == "__main__":
   app.run(debug=True)
--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -13,33 +13,25 @@ from flask_mail import Mail
 from flaskblog.config import Config
 
 
-mail = Mail()
-db = SQLAlchemy()
-bcrypt = Bcrypt()
-login_manager = LoginManager()
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+bootstrap = Bootstrap(app)
+mail = Mail(app)
+
+from flaskblog.users.routes import users
 login_manager.login_view = 'users.login' #function for login route
 login_manager.login_message_category = 'info'
-bootstrap = Bootstrap()
 
 
-def create_app(config_class=Config):
-    app = Flask(__name__)
-    app.config.from_object(Config)
+from flaskblog.posts.routes import posts
+from flaskblog.main.routes import main
+from flaskblog.errors.handlers import errors
 
-    db.init_app(app)
-    mail.init_app(app)
-    bcrypt.init_app(app)
-    bootstrap.init_app(app)
-    login_manager.init_app(app)
+app.register_blueprint(users)
+app.register_blueprint(posts)
+app.register_blueprint(main)
+app.register_blueprint(errors)
 
-    from flaskblog.users.routes import users
-    from flaskblog.posts.routes import posts
-    from flaskblog.main.routes import main
-    from flaskblog.errors.handlers import errors
-
-    app.register_blueprint(users)
-    app.register_blueprint(posts)
-    app.register_blueprint(main)
-    app.register_blueprint(errors)
-
-    return app
